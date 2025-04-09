368f1cff8c2af01448f9d37d41da98f43c0754f9
adk <adk@adk>
2025-04-09 17:26:32 +0200

    Extending parser: unary minus, symbol table, local vars generation, multiple lines

--- Compiler/recursive_descent_parser.py
+++ Compiler/recursive_descent_parser.py
@@ -1,15 +1,18 @@
 from enum import Enum
 
 # TODO:
-# unary minus
-# identifier table
-# functions and conditionals
+# conditionals
+# functions and their arguments + return values
 # https://en.wikipedia.org/wiki/Recursive_descent_parser
 
-input_string = "A=2+(305)*6+7/2+2;"
+input_string = "A=-123.5 + test * 2;\nX=3+5+(2-(3+2));"
 position = 0
 line_number = 1
 current_number = 0
+current_identifier = ""
+code = ""
+
+local_variables = {}  # name-length, in order of occurrence
 
 
 class Symbol(Enum):
@@ -41,21 +44,41 @@ def getchar() -> str:
     return ""
 
 
+def rewind():
+    global position
+    if position > 0:
+        position -= 1
+
+
 def next_symbol():
     global current
     global current_number
-    global position
-    number_buffer = ""
+    global current_identifier
+    global line_number
+
+    buffer = ""
+    buffer_mode = 0  # 1: number 2: identifier
+
     while 1:
         t = getchar()
 
-        if number_buffer:
-            if t.isdigit():
-                number_buffer += t
+        if buffer_mode == 1:
+            if t.isdigit() or t == '.':
+                buffer += t
                 continue
             else:
-                current_number = int(number_buffer)
-                position -= 1
+                current_number = int(buffer) if '.' not in buffer else float(buffer)
+                rewind()
+                return
+        elif buffer_mode == 2:
+            if t.isalnum() or t == '_':
+                buffer += t
+                continue
+            else:
+                current_identifier = buffer
+                if current_identifier not in local_variables:
+                    local_variables[current_identifier] = 1
+                rewind()
                 return
 
         if not t:
@@ -63,13 +86,19 @@ def next_symbol():
             return
         elif t in (' ', '\t'):
             continue
-        elif t.isdigit():
+        elif t == '\n':
+            line_number += 1
+            continue
+        elif t.isdigit() or t == '.':
             current = Symbol.Number
-            number_buffer += t
+            buffer += t
+            buffer_mode = 1
             continue
         elif t.isalnum():
             current = Symbol.Identifier
-            return
+            buffer += t
+            buffer_mode = 2
+            continue
         elif t == "=":
             current = Symbol.Becomes
             return
@@ -111,15 +140,31 @@ def expect(s: Symbol) -> bool:
 
 
 def error(what: str):
+    print(code)
     print(f"Error in line {line_number}: {what}")
     exit(1)
 
 
+def get_variable_offset(name: str) -> int:
+    offs = 0
+    if name not in local_variables:
+        error(f"Unknown variable {name}")
+    for k, v in local_variables.items():
+        if k == name:
+            break
+        offs += v
+    return offs
+
+
 def parse_factor():
+    global code
     if accept(Symbol.Identifier):
-        print("Identifier")
+        code += f"LOAD_LOCAL {get_variable_offset(current_identifier)} ; {current_identifier}\n"
     elif accept(Symbol.Number):
-        print(f"PUSH {current_number}")
+        code += f"PUSH {current_number}\n"
+    elif accept(Symbol.Minus):
+        code += f"PUSH -{current_number}\n"
+        next_symbol()
     elif accept(Symbol.LParen):
         parse_expression()
         expect(Symbol.RParen)
@@ -128,42 +173,59 @@ def parse_factor():
 
 
 def parse_term():
+    global code
     parse_factor()
     while current == Symbol.Mult or current == Symbol.Divide:
         v = current
         next_symbol()
         parse_factor()
-        print("MUL" if v == Symbol.Mult else "DIV")
+        code += "MUL" if v == Symbol.Mult else "DIV"
+        code += '\n'
 
 
 def parse_expression():
-    global current
-    v = current
-    if current == Symbol.Plus or current == Symbol.Minus:
-        next_symbol()
-        if v == Symbol.Minus:
-            print("NEGATE NEXT")
+    global code
+
     parse_term()
     while current == Symbol.Plus or current == Symbol.Minus:
         v = current
         next_symbol()
         parse_term()
-        print("ADD" if v == Symbol.Plus else "SUB")
+        code += "ADD" if v == Symbol.Plus else "SUB"
+        code += '\n'
 
 
 def parse_statement():
+    global code
     if accept(Symbol.Identifier):
+        var = current_identifier
         expect(Symbol.Becomes)
         parse_expression()
+        code += f"STORE_LOCAL {get_variable_offset(var)} ; {var}\n"
+        expect(Symbol.Semicolon)
     else:
         error("parse statement")
 
 
 def parse_block():
-    parse_statement()
+    while 1:
+        parse_statement()
+        if accept(Symbol.EOF):
+            break
+
+
+def generate_preamble():
+    global code
+    txt = ""
+    for k, length in local_variables.items():
+        txt += f"PUSHN {length} ; {k}\n"
+    # todo: optimize into one big block
+    code = txt + code
 
 
 if __name__ == '__main__':
     next_symbol()
     parse_block()
-    expect(Symbol.Semicolon)
+    generate_preamble()
+    print(code)
+    #expect(Symbol.Semicolon)
